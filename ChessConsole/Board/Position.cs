92e3f6abbfd1d00660e0b66c0193a26b937c8835
Alice Vian <alice.b.vian@gmail.com>
2022-07-08 11:36:14 -0300

    Class Position created/checked

---
+++ ChessConsole/Board/Position.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Board
+{
+    internal class Position
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+        public override string ToString()
+        {
+            return $"{Row}, {Column}";
+        }
+    }
+}
--- ChessConsole/Program.cs
+++ ChessConsole/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Board;
+
+namespace ChessConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position P;
+            P = new Position(3, 4);
+
+            Console.WriteLine( "Posição " + P);
+            Console.ReadLine();
+        }
+    }
+}
